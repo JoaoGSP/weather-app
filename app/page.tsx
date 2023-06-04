c06d1a6f3e333b96b58cea810db3ceb13a348b21
João GS Pereira <63313287+JoaoGSP@users.noreply.github.com>
2023-06-04 13:42:32 -0300

    :art: Add the style at the temperature font

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Moon } from './components/visuals/Moon';
 import { Sun } from './components/visuals/Sun';
+import styles from './page.module.css'
+
 export default function Home() {
   return (
     <main>
@@ -9,11 +11,9 @@ export default function Home() {
           <header className='w-full flex justify-end'>
             <button type='button' >Change </button>
           </header>
-          <div className='w-full h-full bg-red-400 flex flex-col justify-evenly'>
-            <section className='bg-blue-50 flex flex-grow justify-center items-center'>
-              <div className='w-2/4 h-full grid place-content-center '>
-                <span className='font-Josefin font-bold text-8xl shadow-xl shadow-red-900'>19º</span>
-              </div>
+          <div className='w-full h-full bg-red-400 flex flex-col items-center justify-evenly'>
+            <section className='w-fit shadow-xl shadow-sky-100 max-h-28 h-fit px-3 pb-2 bg-blue-50 grid place-content-center rounded-xl border-4 border-[#3ac5e3]'>
+              <span className={styles.title}>17º</span>
             </section>
             <section className='bg-blue-100 flex-grow'>2</section>
             <section className='bg-blue-200 flex-grow'>3</section>
