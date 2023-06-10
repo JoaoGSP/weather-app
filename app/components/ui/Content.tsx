d117d31fb6edda53829ef8b867947a63534290ea
Joao G S Pereira <joaogspe@outlook.com>
2023-06-10 11:56:59 -0300

    :construction: Finishing the search flow

--- app/components/ui/Content.tsx
+++ app/components/ui/Content.tsx
@@ -1,53 +1,56 @@
 'use client'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styles from './content.module.css'
 import { montserrat, nunito } from '../../lib/fonts';
 import Toggle from './Toggle';
 import { getWeatherInfo } from '@/app/lib/api';
+import ReactLoading from 'react-loading';
 
 export default function Content() {
-  const [query, setQuery] = useState('')
-  const [lat, setLat] = useState<any>([])
-  const [long, setLong] = useState<any>([])
   const [data, setData] = useState()
+  const [inputValue, setInputValue] = useState('')
 
   useEffect(() => {
     async function fetchData() {
-      navigator.geolocation.getCurrentPosition(position => {
-        setLat(position.coords.latitude)
-        setLong(position.coords.longitude)
-      })
-      const dt = await getWeatherInfo(`${lat},${long}`)
-      if (!data) {
-
-        setData(dt)
-      }
+      getWeatherInfo('brasilia').then(dt => setData(dt))
     }
     fetchData()
-  }, [lat, long])
+  }, [])
+
+  function handleInputChange(event) {
+    const value = event.target.value
+    setInputValue(value)
+  }
 
   async function handleSubmit(e) {
     e.preventDefault()
-    const res = await getWeatherInfo(e.target)
-    const formData = new FormData(res)
-    const formjson = Object.fromEntries(formData.entries())
+    await getWeatherInfo(inputValue).then(res => {
+      setData(res)
+      console.log(data)
+    }).catch(
 
-    console.log(formjson)
+    )
   }
 
   return (
     <>
       <nav className='flex flex-col items-center w-full'>
-        <form method='get' onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className=' p-2 w-96 h-10 bg-transparent rounded-xl border-2 border-white flex align-center justify-between'>
           <input id='scope' title='scope' type='text'
-            value={query}
-            placeholder={data?.location?.name ?? 'Brasilia'}
-            onChange={e => setQuery(e.target.value)}
-            className="w-96 h-10 bg-transparent rounded-xl border border-white focus:outline-none">
+            placeholder={data?.location.name}
+            value={inputValue}
+            onChange={handleInputChange}
+            className="bg-transparent w-full h-full text-sm text-gray-500 font-medium focus:outline-none focus:bg-transparent placeholder:font-normal placeholder:text-gray-500">
           </input>
           <label htmlFor='scope' aria-hidden />
         </form>
+        <button type='submit' className='w-[30px] h-[30px] mt-3 p-1 text-white border-2 rounded-full hover:scale-105'>
+          <svg xmlns="http://www.w3.org/2000/svg" height="100%" fill='currentColor' viewBox="0 -960 960 960" width="100%">
+            <path d="M796-121 533-384q-30 26-69.959 40.5T378-329q-108.162 0-183.081-75Q120-479 120-585t75-181q75-75 181.5-75t181 75Q632-691 632-584.85 632-542 618-502q-14 40-42 75l264 262-44 44ZM377-389q81.25 0 138.125-57.5T572-585q0-81-56.875-138.5T377-781q-82.083 0-139.542 57.5Q180-666 180-585t57.458 138.5Q294.917-389 377-389Z" />
+          </svg>
+        </button>
       </nav>
+
       <div className='flex flex-col w-full items-center'>
         <article style={montserrat.style} className='bg-[#F9FFFE] w-96 h-[450px] z-50 rounded-[50px] shadow-xl flex flex-col items-center py-3 px-5'>
           <header className='w-full flex justify-end py-2 px-4'>
@@ -55,24 +58,24 @@ export default function Content() {
           </header>
           <div className='w-full h-full flex flex-col items-center justify-evenly'>
             <section className='w-fit bg-transparent shadow-xl shadow-sky-100 max-h-28 h-fit px-3 py-2 bg-blue-50 grid place-content-center rounded-xl border-4 border-[#3ac5e3]'>
-              <span style={nunito.style} className={styles.title}>17º</span>
+              <span style={nunito.style} className={styles.title}>{data?.current.temp_c}º</span>
             </section>
             <section className='flex-grow font-medium text-center text-lg mt-8'>
               <p className='text-gray-900'>
-                Beijing
+                {data?.location.name}
               </p>
               <p className='text-gray-900/30'>
-                China
+                {data?.location.country}
               </p>
             </section>
             <section className='text-center flex-grow'>
               <p className='text-gray-900'>Condition</p>
-              <p className='text-gray-900/30'>Clear</p>
+              <p className='text-gray-900/30'>{data?.current.condition.text}</p>
             </section>
           </div>
           <footer>
             <p className='text-gray-900 text-xs font-semibold'>
-              Last Update: <span className='text-gray-900/30 font-light'>02:00h</span>
+              Last Update: <span className='text-gray-900/30 font-light'>{data?.current.last_updated}</span>
             </p>
           </footer>
         </article>
--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,7 +1,5 @@
 async function getWeatherInfo(local?: string): Promise<any> {
-  let localForQuery: string | undefined = 'brasilia'
-  local !== ',' ? localForQuery = local : ''
-  const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=7647ae19db23411c851131213230906&q=${localForQuery}&aqi=no`)
+  const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=7647ae19db23411c851131213230906&q=${local}&aqi=no`)
   if (!res.ok) {
     console.log('Failed to fetch!!');
   }
